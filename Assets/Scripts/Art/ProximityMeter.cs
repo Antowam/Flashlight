7ed57aa6f343dcb0ca0012c287a6b860ac3e8efa
sun boye <k8asnakeload@gmail.com>
2020-01-16 16:33:41 +0100

    daw

---
+++ Assets/Scripts/Art/ProximityMeter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityMeter : MonoBehaviour
+{
+    public Transform target;
+    public PixelationEffect pe;
+
+    [SerializeField] private int _distanceToTarget;
+    [SerializeField] private int _amountToAdd;
+    [SerializeField] private int _startValue;
+    [SerializeField] private int _targetValue;
+    private int  _currentValue;
+
+    void Start() {
+        pe.pixelDensity = _startValue;
+        _currentValue = _startValue;
+    }
+
+    void Update() {
+        if (Vector3.Distance(target.position, transform.position) <= _distanceToTarget) {
+            if (_currentValue >= _targetValue)
+                _currentValue -= _amountToAdd;
+            else
+                _currentValue = _targetValue;
+        }
+        else {
+            if (_currentValue <= _startValue)
+                _currentValue += _amountToAdd;
+            else
+                _currentValue = _startValue;
+        }
+        pe.pixelDensity = _currentValue;
+    }
+}
