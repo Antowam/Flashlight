c6dfb1e4915e22ec3cc7ec3b6c19ebbcc2dd7b13
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2020-01-16 11:05:56 +0100

    Added mainmenucallback script
    Added mainmenucallback script
    added folder for menu scripts

---
+++ Assets/Scripts/Menu/MenuCallBacks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UdpKit;
+
+public class MenuCallBacks : Bolt.GlobalEventListener
+{
+    //What happens when you click the host server button
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    //What happens when you click join random server button
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    //What happens when start server is done
+    public override void BoltStartDone()
+    {
+        //If it was called from the host of the game
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = "Its a match";
+
+            BoltNetwork.SetServerInfo(matchName, null);
+            BoltNetwork.LoadScene("SampleScene");
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach(var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if(photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+}
