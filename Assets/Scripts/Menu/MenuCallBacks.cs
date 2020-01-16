6768a955a564279187234b40b649bdfa61371c0a
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2020-01-16 14:27:52 +0100

    Made a somewhat functional server list
    Made a somewhat functional server list
    cant scroll in the server list
    server list takes server names

--- Assets/Scripts/Menu/MenuCallBacks.cs
+++ Assets/Scripts/Menu/MenuCallBacks.cs
@@ -1,15 +1,25 @@
-﻿using Bolt.Matchmaking;
+﻿using System.Collections.Generic;
+using Bolt.Matchmaking;
 using System;
 using UdpKit;
 using UnityEngine.UI;
+using UnityEngine;
 using TMPro;
 
 public class MenuCallBacks : Bolt.GlobalEventListener
 {
     public Button serverJoinGameButton;
+    public GameObject serverListPanel;
+    private List<Button> joinServerButtonList = new List<Button>();
+    private float buttonSpacing = -100.0f;
 
     public string serverName = "";
     public TextMeshProUGUI serverNameText;
+
+    private void Awake()
+    {
+        Screen.fullScreen = false;
+    }
 
     //What happens when you click the host server button
     public void StartServer()
@@ -44,30 +54,47 @@ public class MenuCallBacks : Bolt.GlobalEventListener
 
     public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
     {
+        ClearServerList();
         foreach(var session in sessionList)
         {
             UdpSession photonSession = session.Value as UdpSession;
+            UdpSession photonSessionOld = photonSession;
 
-            /* this joins the first session
-            if(photonSession.Source == UdpSessionSource.Photon)
-            {
-                BoltNetwork.Connect(photonSession);
-            }
-            */
+            Button joinGameButtonClone = Instantiate(serverJoinGameButton);
+            joinGameButtonClone.transform.SetParent(serverListPanel.transform);
+            joinGameButtonClone.transform.localScale = new Vector3(1, 1, 1);
+            joinGameButtonClone.transform.localPosition = new Vector3(0, buttonSpacing * joinServerButtonList.Count, 0);
+            joinGameButtonClone.GetComponentInChildren<TextMeshProUGUI>().text = photonSession.HostName;
+
+            joinGameButtonClone.onClick.AddListener(() => JoinGame(photonSession));
+
+            joinServerButtonList.Add(joinGameButtonClone);
+
+            //this joins the first session
+            //if (photonSession.Source == UdpSessionSource.Photon)
+            //{
+            //    BoltNetwork.Connect(photonSession);
+            //}
         }
     }
 
-    public void QuickJoin(Map<Guid, UdpSession> sessionList)
+    public void JoinGame(UdpSession photonSession)
     {
-        foreach (var session in sessionList)
-        {
-            UdpSession photonSession = session.Value as UdpSession;
+        BoltNetwork.Connect(photonSession);
+    }
 
-            //This connects to the first one found
-            if(photonSession.Source == UdpSessionSource.Photon)
-            {
-                BoltNetwork.Connect(photonSession);
-            }
+    private void ClearServerList()
+    {
+        foreach(Button button in joinServerButtonList)
+        {
+            Destroy(button.gameObject);
         }
+
+        joinServerButtonList.Clear();
+    }
+
+    public void Back()
+    {
+        BoltLauncher.Shutdown();
     }
 }
