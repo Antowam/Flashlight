290f87bd765508c535746334b672db764282697b
hydrarguru <henrikengqvist99@gmail.com>
2020-01-16 15:40:07 +0100

    Simple Flashlight

---
+++ Assets/Scripts/Player/Flashlight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashLight;
+    private bool isLightOn = false;
+
+    private void Awake()
+    {
+        flashLight.enabled = false;
+    }
+
+    private void Update()
+    {
+        HandleFlashLight(KeyCode.E);
+    }
+
+    void HandleFlashLight(KeyCode key)
+    {
+        if(Input.GetKeyDown(key) && !isLightOn)
+        {
+            flashLight.enabled = true;
+            isLightOn = true;
+        }
+        else if(Input.GetKeyDown(key) && isLightOn)
+        {
+            flashLight.enabled = false;
+            isLightOn = false;
+        }
+    }
+}
