496e40b384fa4bde642150a5ee51579729a51db0
sun boye <k8asnakeload@gmail.com>
2020-01-16 12:21:05 +0100

    daw

---
+++ Assets/Scripts/Menu/TestMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float movSpeed;
+    public float rotSpeed;
+    private Vector3 rotateValue;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float y = Input.GetAxisRaw("Mouse X");
+        float x = Input.GetAxisRaw("Mouse Y");
+
+        rotateValue = new Vector3(x, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += transform.forward * movSpeed;
+        }
+    }
+}
