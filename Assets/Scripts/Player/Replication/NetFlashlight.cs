53ea78c3138ddc1f6f233567e6386644084daa36
sun boye <k8asnakeload@gmail.com>
2020-01-17 14:40:47 +0100

    daw

---
+++ Assets/Scripts/Player/Replication/NetFlashlight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class NetFlashlight : Bolt.EntityBehaviour<ICustomPlayerState>
+{
+    public Light flashLight;
+    private bool isLightOn = false;
+
+    public override void Attached()
+    {
+        flashLight.enabled = false;
+        flashLight.intensity = 2f;
+        flashLight.spotAngle = 65f;
+    }
+
+    public override void SimulateOwner()
+    {
+        HandleFlashLight(KeyCode.E);
+    }
+
+    void HandleFlashLight(KeyCode key)
+    {
+        if(Input.GetKeyDown(key) && !isLightOn)
+        {
+            flashLight.enabled = true;
+            isLightOn = true;
+        }
+        else if(Input.GetKeyDown(key) && isLightOn)
+        {
+            flashLight.enabled = false;
+            isLightOn = false;
+        }
+    }
+}
