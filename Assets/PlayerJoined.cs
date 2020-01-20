05f454609d1c498d3eda30800b44a2fb9af8f936
sun boye <k8asnakeload@gmail.com>
2020-01-20 15:19:39 +0100

    dwa

---
+++ Assets/PlayerJoined.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoined : Bolt.EntityBehaviour<ICustomPlayerState>
+{
+    public Camera entityCamera;
+    private string username;
+
+    /*
+    public override void Attached() {
+        if(entity.IsOwner) {
+            entityCamera.gameObject.SetActive(true);
+        }
+    }*/
+
+    private void Update() {
+        if (entity.IsOwner && entityCamera.gameObject.activeInHierarchy == false) {
+            entityCamera.gameObject.SetActive(true);
+        }
+    }
+}
