82578d1824f8b7fba8e7eaf14aa6bcd4449331bc
hydrarguru <henrikengqvist99@gmail.com>
2020-01-22 10:24:03 +0100

    Flashlight
    Can detect objects with raycast
    added a option for flicker off/on
    added drainamount for non hardcoded value.

--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -6,12 +6,15 @@ public class Flashlight : MonoBehaviour
 {
     PlayerController controller;
     FlashlightFlickerEffect flickerEffect;
-
     public Light flashLight;
 
     private bool isLightOn = false;
     public float maxBattery = 100f;
     private float currentBattery;
+    public bool flicker;
+
+    [Tooltip("Drain amount for the flashlight battery")]
+    public float drainAmount;
 
 
 
@@ -19,26 +22,33 @@ public class Flashlight : MonoBehaviour
     private void Awake()
     {
         flickerEffect = GetComponent<FlashlightFlickerEffect>();
-        flickerEffect.enabled = false;
+        SetFlicker();
 
         currentBattery = maxBattery;
         flashLight.enabled = false;
         flashLight.intensity = 2f;
         flashLight.spotAngle = 65f;
+        flashLight.range = 10f;
     }
 
     private void Update()
     {
         if (isLightOn)
         {
-            DrainBattery(4f);
-            Debug.Log("Battery: " + currentBattery);
+            DrainBattery(drainAmount);
         }
+
         HandleFlashLight(KeyCode.E);
+        
     }
 
     void HandleFlashLight(KeyCode key)
     {
+        if(Input.GetKeyDown(KeyCode.Q) && isLightOn)
+        {
+            DetectEnemy();
+        }
+
         if(Input.GetKeyDown(key) && !isLightOn)
         {
             flashLight.enabled = true;
@@ -57,6 +67,33 @@ public class Flashlight : MonoBehaviour
         if (currentBattery < 35)
         {
             flickerEffect.enabled = true;
+        }
+        if(currentBattery <= 0)
+        {
+            isLightOn = false;
+        }
+    }
+
+    void SetFlicker()
+    {
+        if (flicker)
+        {
+            flickerEffect.enabled = true;
+        }
+        else
+        {
+            flickerEffect.enabled = false;
+        }
+    }
+
+    void DetectEnemy()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, flashLight.range))
+        {
+            Debug.DrawRay(transform.position, Vector3.forward, Color.red);
+            Debug.Log("Did Hit " + hit.collider);
         }
     }
 }
